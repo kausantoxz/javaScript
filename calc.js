41b885a61f7b46c801577e0bd80807d597d36e1f
kausantoxz <kausantox2007@gmail.com>
2023-09-21 11:31:19 -0300

    criaçao de modelo

--- calc.js
+++ calc.js
@@ -9,3 +9,36 @@ function adicao() {
 
 }
  
+function subtracao() {
+    let n1 = Number(window.prompt("digite o primeiro número:"))
+    let n2 = Number(window.prompt("digite o segundo número:"))
+   n3= n1-n2
+
+   let res = document.getElementById('resultado')
+
+   res.innerHTML = `<p> o resultado da soma é ${n3}.`
+
+}
+ 
+function multiplicacao() {
+    let n1 = Number(window.prompt("digite o primeiro número:"))
+    let n2 = Number(window.prompt("digite o segundo número:"))
+   n3= n1*n2
+
+   let res = document.getElementById('resultado')
+
+   res.innerHTML = `<p> o resultado da soma é ${n3}.`
+
+}
+ 
+ 
+function divisao() {
+    let n1 = Number(window.prompt("digite o primeiro número:"))
+    let n2 = Number(window.prompt("digite o segundo número:"))
+   n3= n1/n2
+
+   let res = document.getElementById('resultado')
+
+   res.innerHTML = `<p> o resultado da soma é ${n3}.`
+
+}
